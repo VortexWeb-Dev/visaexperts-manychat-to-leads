7a3b38be9400bec9e14f204103045a1fe6b84afb
fasilofficial <muhammedfasilofficial@gmail.com>
2024-11-04 17:54:05 +0530

    main logic added

--- crest/settings.php
+++ crest/settings.php
@@ -1,3 +1,3 @@
 <?php
-define('C_REST_WEB_HOOK_URL','');
+define('C_REST_WEB_HOOK_URL','https://visaexperts.bitrix24.com/rest/292290/xgwon5iwyw4xxq4y/');
 define('C_REST_BLOCK_LOG',true);
--- handler.php
+++ handler.php
@@ -1,39 +1,32 @@
 <?php
 require_once(__DIR__ . '/crest/crest.php');
 
-function logToFile($filename, $data)
-{
-    file_put_contents('logs/' . $filename, print_r($data, true), FILE_APPEND);
-}
-
-function getRequestData()
-{
-    $input = file_get_contents('php://input');
-    return json_decode($input, true);
-}
-
-function handlePostRequest($data)
-{
-    if (!empty($data)) {
-        logToFile('log.log', $data);
-
-
-    } else {
-        logToFile('err.log', 'Invalid or empty JSON');
-        echo json_encode(['status' => 'error', 'message' => 'Invalid or empty JSON']);
-        exit;
-    }
-}
-
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = getRequestData();
-    handlePostRequest($data);
+    $input = file_get_contents("php://input");
+    $data = json_decode($input, true);
+    
+    file_put_contents('logs/log.log', print_r($data, true), FILE_APPEND);
+    
+    $name = $data['name'] ?? '';
+    $phone = $data['whatsapp_phone'] ?? '';
+    $preferred_country = $data['last_input_text'] ?? '';
+
+    $formData = [
+        'TITLE' => $name . ' - ManyChat',
+        'NAME' => $name,
+        'PHONE' => [['VALUE' => $phone, 'VALUE_TYPE' => 'WORK']],
+        'UF_CRM_LEAD_1679811828816' => ($preferred_country == 'Poland') ? 602 : (($preferred_country == 'Czech Republic') ? 604 : ''),
+        'SOURCE_ID' => '20|ANK_CHATS_APP24_WHATSAPP',
+    ];
+
+    $result = CRest::call('crm.lead.add', [
+        'fields' => $formData,
+    ]);
+
+    file_put_contents('logs/res.log', print_r($result, true), FILE_APPEND);
+    echo json_encode(['status' => 'success', 'result' => $result]);
+
 } else {
-    header('Content-Type: application/json');
-    header('HTTP/1.1 405 Method Not Allowed');
-    $errorMessage = ['status' => 'error', 'message' => 'Only POST requests are allowed'];
-    echo json_encode($errorMessage);
-    logToFile('err.log', $errorMessage);
-    exit;
+    echo json_encode(['status' => 'error', 'message' => 'Not a valid method']);
 }
+
