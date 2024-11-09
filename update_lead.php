283b5acaceef99e4f7e2c840473185f62f30dabe
fasilofficial <muhammedfasilofficial@gmail.com>
2024-11-09 17:17:43 +0530

    update update_lead, crest/settings

--- update_lead.php
+++ update_lead.php
@@ -4,12 +4,10 @@ require_once(__DIR__ . '/crest/crest.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $input = file_get_contents("php://input");
     $data = json_decode($input, true);
-    
-    file_put_contents('logs/update_log.log', print_r($data, true), FILE_APPEND);
-    
-    $id = $_GET['id'] ?? '';
-    
 
+    file_put_contents('logs/update_log.log', print_r($data, true), FILE_APPEND);
+
+    $id = $data['custom_fields']['lead_id'] ?? '';
     $dream_destination = $data['custom_fields']['dream_destination'] ?? '';
     $has_valid_passport = $data['custom_fields']['has_valid_passport'] ?? 0;
 
@@ -22,14 +20,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'STATUS_ID' => 'UC_NJRLRN',
     ];
 
-    $result = CRest::call('crm.lead.update', [
-        'id' => $id,
-        'fields' => $formData,
-    ]);
+    if (!empty($id)) {
 
-    file_put_contents('logs/update_res.log', print_r($result, true), FILE_APPEND);
-    echo json_encode(['status' => 'success', 'result' => $result]);
+        $result = CRest::call('crm.lead.update', [
+            'id' => $id,
+            'fields' => $formData,
+        ]);
 
+        file_put_contents('logs/update_res.log', print_r($result, true), FILE_APPEND);
+        echo json_encode(['status' => 'success', 'result' => $result]);
+    }
 } else {
     echo json_encode(['status' => 'error', 'message' => 'Not a valid method']);
 }
